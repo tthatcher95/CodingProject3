ee74c4c355b40f6593bf886c2e9eb92544dca702
shortykg <swtkaitlyn@gmail.com>
2019-04-02 20:16:07 -0700

    Added Test Outline

---
+++ tests/testthat/test-NNetIterations.R
@@ -0,0 +1,26 @@
+# write tests for that R function, in tests/testthat/test-NNetIterations:
+# (1) for valid inputs your function returns an output of the expected type/dimension
+# (2) for an invalid input, your function stops with an informative error message.
+
+library(CodingProject3)
+library(testthat)
+context("test-NNetIterations")
+
+
+test_that("NNetIterations computes the right demensions", {
+  # data(spam, package = "ElemStatLearn")
+  # X.mat<-spam[1:100,-58]
+  # X.scaled.mat<-scale(X.mat, center = TRUE, scale = TRUE)
+  # y.vec<-spam[1:100, 58]
+
+  # expect_equal(nrow(res), 1)
+})
+
+test_that("NNetIterations throws errors", {
+  # data(spam, package = "ElemStatLearn")
+  # X.mat<-spam[1:100,-58]
+  # X.scaled.mat<-scale(X.mat, center = TRUE, scale = TRUE)
+  # y.vec<-spam[1:100, 58]
+  
+  # expect_error()
+})
